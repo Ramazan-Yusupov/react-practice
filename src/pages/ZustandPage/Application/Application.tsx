42f523cadd7ba4e1ea8a34f9dcfd67e962fa938d
Ramazan <frontenddev747@gmail.com>
2025-11-24 11:50:48 +0300

    Упрощено использование компонента CardApp, удалены ненужные пропсы и логика счетчика.

--- src/pages/ZustandPage/Application/Application.tsx
+++ src/pages/ZustandPage/Application/Application.tsx
@@ -1,18 +1,9 @@
-import { useStoreApp } from "@/store/useStoreApp";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { CardApp } from "./ui/CardApp";
 
 export function Application() {
-  const { counter, increment, decrement } = useStoreApp();
   return (
     <div className="inline-flex flex-col gap-5">
-      <CardApp
-        counter={counter}
-        onClick={decrement}
-        onClick2={increment}
-        children={<AiOutlineMinus />}
-        children2={<AiOutlinePlus />}
-      />
+      <CardApp>Frontend</CardApp>
     </div>
   );
 }
--- src/pages/ZustandPage/Application/ui/CardApp.tsx
+++ src/pages/ZustandPage/Application/ui/CardApp.tsx
@@ -1,27 +1,11 @@
-import { Button } from "@/shared/ui/Button";
-
 interface CardProps {
-  counter: number;
-  onClick: () => void;
-  onClick2: () => void;
   children: React.ReactNode;
-  children2: React.ReactNode;
 }
 
-export function CardApp({
-  counter,
-  onClick,
-  onClick2,
-  children,
-  children2,
-}: CardProps) {
+export function CardApp({ children }: CardProps) {
   return (
     <div className="border-2 p-3 rounded-2xl">
-      <div className="flex items-center gap-5">
-        <Button onClick={onClick}>{children}</Button>
-        <div>Counter: {counter}</div>
-        <Button onClick={onClick2}>{children2}</Button>
-      </div>
+      <div className="">{children}</div>
     </div>
   );
 }
