f0969bca433acb0606a123bb16ee727a14256f97
Frontend <frontenddev747@gmail.com>
2026-01-28 10:57:35 +0300

    add

---
+++ src/Archive/FilterItemsArchive.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/shared/ui/Input";
+import { useState } from "react";
+import { devsMock } from "@/mockApi/itemsMock";
+import { CodeBlock } from "@/components/CodeBlock";
+
+export function FilterItemsArchive() {
+  const [query, setQuery] = useState("");
+
+  const filteredItems = devsMock.filter(
+    (item) =>
+      item.dev.toLowerCase().includes(query.toLowerCase()) ||
+      item.lang.toLowerCase().includes(query.toLowerCase()),
+  );
+  return (
+    <>
+      <Input
+        value={query}
+        placeholder="Search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {filteredItems.map((item) => (
+        <CodeBlock
+          isBordered
+          key={item.id}
+          codeL={item.lang}
+          codeTitle={item.dev}
+        />
+      ))}
+    </>
+  );
+}
