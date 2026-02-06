fc6a1e7d7aa20ec4a0c3c0740268ce51770d8c19
Frontend <frontenddev747@gmail.com>
2026-02-06 14:27:52 +0300

    add

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-practice">
       <Provider store={store}>
         <App />
       </Provider>
