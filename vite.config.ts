9abec76ff0fb867afeea95f957de0c73224489cf
Frontend <frontenddev747@gmail.com>
2026-02-06 14:12:22 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const isProd = mode === "production";
   return {
-    base: isProd ? "/home" : "/",
+    base: isProd ? "/react-practice" : "/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
