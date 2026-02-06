87f5b1906b46a0e53700387c36c00fa08074faa1
Frontend <frontenddev747@gmail.com>
2026-02-06 13:35:48 +0300

    add

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,8 @@
 import { Card } from "@/shared/ui/Card";
-import { UserList } from "@/Folders/components/UserList";
+
 import { CodeBlock } from "@/shared/ui/CodeBlock";
-import { useDispatch, useSelector } from "react-redux";
-import { type AppDispatch, type RootState } from "@/store/store";
-import { useEffect, useState } from "react";
-import { fetchUsers, selectSortedUsers, setSortBy } from "@/store/userSlice";
-import { LoadingUI } from "@/shared/ui/LoadingUI";
-import { ErrorUI } from "@/shared/ui/ErrorUI";
-import { FlexContainer } from "@/shared/ui/FlexContainer";
-import { Button } from "@/shared/ui/Button";
-import { Input } from "@/shared/ui/Input";
-import { useFilteredUsers } from "@/Folders/hooks/useFilteredUsers";
 
 export default function Home() {
-  const dispatch = useDispatch<AppDispatch>();
-  const users = useSelector(selectSortedUsers);
-  const { loading, error, sortBy } = useSelector(
-    (state: RootState) => state.users,
-  );
-
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const filteredUsers = useFilteredUsers(users, searchTerm);
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
-
-  const handleSortChange = (newSort: "name" | "email" | "username") => {
-    dispatch(setSortBy(newSort));
-  };
   return (
     <div>
       <Card
@@ -41,34 +14,6 @@ export default function Home() {
         maxHeight={636}
       >
         <CodeBlock isBordered codeL="tsx" codeTitle="Frontend" />
-        <Input
-          value={searchTerm}
-          placeholder="Search..."
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-
-        <FlexContainer justify="between">
-          <Button
-            title="Sort by Name"
-            disabled={sortBy === "name"}
-            onClick={() => handleSortChange("name")}
-          />
-          <Button
-            title="Sort by Email"
-            disabled={sortBy === "email"}
-            onClick={() => handleSortChange("email")}
-          />
-          <Button
-            title="Sort by Username"
-            disabled={sortBy === "username"}
-            onClick={() => handleSortChange("username")}
-          />
-        </FlexContainer>
-
-        {loading && <LoadingUI isLoading={loading} />}
-        {error && <ErrorUI isError={error} />}
-
-        <UserList users={filteredUsers} />
       </Card>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src"),
-      // ... остальные алиасы
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  return {
+    base: isProd ? "/react-practice/" : "/",
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
