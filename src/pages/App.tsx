d8460718795fd4f4f830c5b6a9d6ef33df3e41ac
Frontend <frontenddev747@gmail.com>
2026-03-06 10:28:34 +0300

    add

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { Layout } from "../Layout";
 import { Home } from "./Home/Home";
 import { BlockCode } from "./BlockCode/BlockCode";
+import { ROUTES } from "@/routes";
 
 export function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/BlockCode" element={<BlockCode />} />
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.BLOCK_CODE} element={<BlockCode />} />
       </Routes>
     </Layout>
   );
---
+++ src/routes/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  HOME: "/",
+  BLOCK_CODE: "/BlockCode",
+};
