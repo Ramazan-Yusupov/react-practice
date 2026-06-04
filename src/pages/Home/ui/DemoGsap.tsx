fd01318edc387e5801b2d35642b5741b2cff3115
Ramazan <frontenddev747@gmail.com>
2026-06-04 12:35:46 +0300

    changed size

--- src/pages/Home/ui/DemoGsap.tsx
+++ src/pages/Home/ui/DemoGsap.tsx
@@ -36,7 +36,7 @@ export function DemoGsap() {
   return (
     <Box ref={root} className="relative flex flex-col gap-10">
       <Box
-        size={130}
+        size={120}
         text="fromTo()"
         className="sceneTo1"
         background="#014422"
